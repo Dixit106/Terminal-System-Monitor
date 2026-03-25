558d08478a944d2085f22013c46cf677f4c30455
Dixit106 <dixitk2958@gmail.com>
2026-03-25 18:13:23 +0530

    learning widgets

--- dashboard.py
+++ dashboard.py
@@ -1,30 +1,14 @@
-from textual.app import App
-from textual import events
+#widgets learining
+from textual.app import App, ComposeResult
+from textual.widgets import Welcome
 
+class WelcomeApp(App):
+    def compose(self) -> ComposeResult:
+        yield Welcome()
 
-class EventApp(App):
-
-    COLORS = [
-        "white",
-        "maroon",
-        "red",
-        "purple",
-        "fuchsia",
-        "olive",
-        "yellow",
-        "navy",
-        "teal",
-        "aqua",
-    ]
-
-    def on_mount(self) -> None:
-        self.screen.styles.background = "darkblue"
-
-    def on_key(self, event: events.Key) -> None:
-        if event.key.isdecimal():
-            self.screen.styles.background = self.COLORS[int(event.key)]
-
+    def on_button_pressed(self) -> None:
+        self.exit()
 
 if __name__ == "__main__":
-    app = EventApp()
-    app.run()
+    app = WelcomeApp()
+    app.run()        
