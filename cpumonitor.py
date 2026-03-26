40c13e66517ad5056180a412838a66d4d58272d3
Dixit106 <dixitk2958@gmail.com>
2026-03-26 20:18:52 +0530

    cpu monitor end but there are errors

---
+++ cpumonitor.py
@@ -0,0 +1,22 @@
+import psutil
+from textual.app import App, ComposeResult
+from textual.widgets import Static 
+
+class CPUMonitor(Static):
+
+    def on_mount(self) -> None:
+        self.update_timer = self.set_interval(1.0, self.update_cpu)
+
+    def update_cpu(self) -> None:
+        cpu_percent = psutile.cpu_percent()
+
+        self.update(f"CPU Usage: {cpu_percent%")
+
+class CPUApp(App):
+    def compose(self) -> ComposeResult:
+        yield CPUMonitor()
+
+if __name__ == "__main__":
+    app = CPUApp()
+    app.run()
+                        
