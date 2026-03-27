19c912023c1048f4f5d1ee19ab2e897a0de906a5
Dixit106 <dixitk2958@gmail.com>
2026-03-27 13:33:57 +0530

    dashboard file import and importing all tools i made

--- dashboard.py
+++ dashboard.py
@@ -1,18 +1,10 @@
-#Exiting
-from textual.app import App, ComposeResult
-from textual.widgets import Label, Button
+#importing
+from textual.app import App, ComposeResult 
+from textual.widgets import Header, Footer 
+from textual.containers import Grid 
 
-class QuestionApp(App[str]):
-    def compose(self) -> ComposeResult:
-        yield Label("Do you Like Textual?")
-        yield Button("Yup" ,id="yes", variant="primary")
-        yield Button("No" ,id="no", variant="error")
-
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        self.exit(event.button.id)
-
-if __name__ == "__main__":
-    app = QuestionApp()
-    reply = app.run()
-    print(reply)
-                
+#putting all tools i built here
+from stopwatch import Stopwatch 
+from cpumonitor import BRAINTracker
+from rammonitor import RAMMonitor 
+from asciiart import AnimeArt
