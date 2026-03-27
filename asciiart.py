f6c91c6bb7cedde157ee7a0f73c94912916cbea1
Dixit106 <dixitk2958@gmail.com>
2026-03-27 11:17:41 +0530

    asciiart import and figure kinda thing like visual

---
+++ asciiart.py
@@ -0,0 +1,17 @@
+from textual.app import App, ComposeResult 
+from textual.widgets import Static 
+
+#creating frames that we can see
+FRAME_1 = """
+/\\_/\\
+(0 . 0)
+>  ^  <
+(     )
+  """
+
+FRAME_2 = """
+/\\_/\\
+(- . -)
+>  ^  <
+(     )
+  """
